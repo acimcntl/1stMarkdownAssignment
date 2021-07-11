f741df3521b240dc56bcac904ebeda31a38bd7d5
KoiBaplog <jed.sanluis@obf.ateneo.edu>
2021-07-11 20:44:07 +0800

    Sana talaga tama

---
+++ Sana Tama.R
@@ -0,0 +1,31 @@
+---
+  title: "First Markdown"
+author: "KoiBaplog"
+date: "7/10/2021"
+output: html_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Jed Matthew San Luis
+
+## Aside from the reasons mentioned in class, why do we need to study and analyze data?
+
+Data science should be taught and appreciated by everyone, for data is crucial in most if not everything people do. As mentioned in class, with the rise of the internet, the numbers of data produced have skyrocketed. Everyone needs to be well-versed in data science because having clean and understandable data for people to consume can lead everyone to make informed decisions. The ability to analyze data would make people less vulnerable to scams and less gullible to false information. This, in turn, would make the internet a safer and less vulnerable place for those that are familiar and well-versed in the field of data science.
+
+## Cite at least one instance or application where data science was used and contributed on policy-making, day-to-day decisions, knowing market trends, and scientific researches.
+
+As the pandemic gave me lots of time for manifesting new hobbies and harboring new interests, I have started to be inclined with stocks and cryptocurrency trading. With this, I have started to observe that in trading and interacting with both markets, there is an undeniably large amount of data to process. Data science is evident through this as large amounts of data are cleaned and summarized for beginner-friendly and professional consumption. With data science, the prediction of market trends, the flooring, and ceiling prices for various stocks or cryptocurrency coins are made possible and accurate. About another one of my hobbies, I have also been more active in different video games. I observed that there are also traces of data science in this field of entertainment. There are a lot of data that are cleaned and analyzed by numerous analyst to provide a tier list which determines the "Most Effective Tactics Available" or META that gives an edge to the players. Overall, some various guides and researches solely rely on the data provided by large player bases to determine the standards set in particular games.
+
+## Propose at least one data science topic that you want to pursue: Have a broad description of the topic, describe the availability of the data, what kinds of statistical method you think you will need, and who would benefit this study.
+
+In proposing any topic for any research, I often believe that it is crucial to have a personal interest in the research to be pursued. With this, I would like to propose the topic of data science's importance in different video games, particularly those for competitive play such as League of Legends and Valorant. Both games have a large player base and constantly receive updates that tend to change the standards in their gameplay. The data is being manifested in both games through the players of each game. The data also varies for each patch or update received by the games because it comes with "nerfs" (debuffs), buffs, adjustments, and other changes to different elements within the game. I believe that the large player bases of the mentioned games would greatly benefit from this study. Clean data that could be easily interpreted would help them understand the various METAs more, thus, giving them a greater chance to improve their gameplay and individual performance with statistics. I believe that interpreting and analyzing these data would require descriptive and inferential statistics, depending on the specific data to be discussed as the topic is broad. Whether their goal is to climb through the rankings, or individually be better at the game, or even to simply enjoy the game, the study would benefit them.
+
+## Sources:
+Ho, L. (n.d.). Explainer: What Is A Meta? Arc UNSW Student Life. https://www.arc.unsw.edu.au/blitz/read/explainer-what-is-a-metaquestion. 
+
+https://www.proguides.com
+
+Chappelow, J. (2021, May 19). Statistics Definition. Investopedia. https://www.investopedia.com/terms/s/statistics.asp. 
